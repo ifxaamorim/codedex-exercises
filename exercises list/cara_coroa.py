0d7069a9a45dd4eb80352d6b4267e642b5448f26
ifxaamorim <reservaamorim47@gmail.com>
2024-12-24 15:58:40 -0300

    Atividades de condições if/else

---
+++ exercises list/cara_coroa.py
@@ -0,0 +1,8 @@
+import random
+
+num = random.randint(0, 1) # Gera o número aleatoriamente
+
+if num > 0.5:
+    print("cara")
+else:
+    print("coroa")
---
+++ exercises list/notas.py
@@ -0,0 +1,6 @@
+notas = int(input("Insira sua nota: "))
+
+if notas >= 65:
+    print("Você está aprovado")
+else:
+    print("Você está reprovado")
