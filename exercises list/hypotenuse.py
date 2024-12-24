89313d3ae8621401f4da832e708123fffbf78265
ifxaamorim <reservaamorim47@gmail.com>
2024-12-23 23:41:24 -0300

    Atividade de teorema de pitágoras

---
+++ exercises list/hypotenuse.py
@@ -0,0 +1,9 @@
+# Crie um programa que peça ao usuário dois números, ae b, e então calcule a hipotenusa c.
+
+valor_1 = int(input("Insira o valor de a: "))
+
+valor_2 = int(input("Insira o valor de b: "))
+
+hipotenusa =   (valor_1 ** 2 +  valor_2 ** 2) ** 0.5
+
+print(hipotenusa)
