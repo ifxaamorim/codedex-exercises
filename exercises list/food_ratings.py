e90a397d937e7ebcde85d83cc057c9b1d31f4a44
ifxaamorim <reservaamorim47@gmail.com>
2024-12-25 17:30:06 -0300

    Alteração do código

--- exercises list/food_ratings.py
+++ exercises list/food_ratings.py
@@ -1,12 +1,12 @@
 nota = float (input("Insira sua avaliação (0 - 5): "))
 
-if nota >= 4.5:
+if nota > 4.5:
     print("Extradordinário")
-elif nota >= 4:
+elif nota > 4:
     print("Excelente")
-elif nota >= 3:
+elif nota > 3:
     print("Bom")
-elif nota >= 2:
+elif nota > 2:
     print("Razoável")
 else:
     print("Ruim")
