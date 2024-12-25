816ad44e80ac7575858e58b335eaead5db00bad1
ifxaamorim <reservaamorim47@gmail.com>
2024-12-25 17:26:07 -0300

    Exercicio avaliação de restaurante

---
+++ exercises list/food_ratings.py
@@ -0,0 +1,12 @@
+nota = float (input("Insira sua avaliação (0 - 5): "))
+
+if nota >= 4.5:
+    print("Extradordinário")
+elif nota >= 4:
+    print("Excelente")
+elif nota >= 3:
+    print("Bom")
+elif nota >= 2:
+    print("Razoável")
+else:
+    print("Ruim")
