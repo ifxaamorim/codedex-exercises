633fa8604e934e6c00f8e8f72ca0f34c7ff1a3a9
ifxaamorim <reservaamorim47@gmail.com>
2024-12-28 21:39:21 -0300

    Exercicio sobre interpolação de strings

---
+++ exercises list/99_garrafas.py
@@ -0,0 +1,5 @@
+for garrafa in range(99, 0, -1):
+    print(f"{garrafa} garrafas de cerveja na parede")
+    print(f"{garrafa} garrafas de cerveja")
+    print("Pegue uma, passe adiante")
+    print(f"{garrafa - 1} garrafas de cerveja na parede")
