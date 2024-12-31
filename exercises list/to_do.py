4bccac955392c18b82bb1ce4543255600cc34ac2
ifxaamorim <reservaamorim47@gmail.com>
2024-12-30 23:52:37 -0300

    Uso do index em listas

---
+++ exercises list/to_do.py
@@ -0,0 +1,3 @@
+to_do = ["🏦 Get quarters", "🧺 Do laundry.", "🌳 Take a walk.", "💈 Get a haircut.", "🍵 Make some tea.", "💻 Complete Lists chapter."
+         "💖 Call mom.", "📺 Watch My Hero Academia." ]
+print(to_do[2:5])
