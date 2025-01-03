3ec2a94d4f95bbd15f2f560bae4474fdfcae1a16
ifxaamorim <reservaamorim47@gmail.com>
2025-01-03 18:06:36 -0300

    Desafio 2 - Loops: Ano novo

---
+++ exercises list/ano_novo.py
@@ -0,0 +1,7 @@
+## Desaio - 2: Loops 
+## Contagem regressiva de 10 a 1 em cada linha e, em seguida, uma mensagem de Feliz Ano Novo no final. 
+
+for new_year in range(10, 0, -1):
+  print(new_year)
+if new_year == 1:
+  print("Feliz ano novo ╰(*°▽°*)╯")
