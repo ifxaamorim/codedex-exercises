cb327d1eab7d008907e503a6c800904db00394d2
ifxaamorim <reservaamorim47@gmail.com>
2025-01-05 19:33:42 -0300

    Desafio 1 - Já chegamos. v2

--- exercises list/ja_chegamos.py
+++ exercises list/ja_chegamos.py
@@ -1,3 +1,8 @@
+# Desafio: 1 - Já chegamos?
+
+# Pergunte ao usuário “Já chegamos?”. Em seguida pergunte ao usuário “Já chegamos?” repetidamente até que ele responda “Sim”.
+
+
 answer = input("Já chegamos?: ")
 
 while answer != "Sim":
