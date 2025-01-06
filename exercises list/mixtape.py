d008fe3990e171e2beba3c8b0c59eb3dde81bd53
ifxaamorim <reservaamorim47@gmail.com>
2025-01-06 20:40:59 -0300

    Exercicío sobre uso de loops em lista

---
+++ exercises list/mixtape.py
@@ -0,0 +1,15 @@
+## Crie um programa mixtape.py e pense em um tema para as músicas. Faça uma lista playliste coloque algumas músicas.
+
+# Músicas pop
+
+playlist = [
+    "BIRDS OF A FEATHER - Billie Eilish",
+    "That s So True - Gracie Abrams",
+    "As It Was - Harry Styles",
+    "brutal - Olivia Rodrigo",
+    "LIGHTS ON - Tyler, The Creator",
+    "Illusion - Dua Lipa"
+]
+
+for musicas in playlist:
+    print(musicas)
