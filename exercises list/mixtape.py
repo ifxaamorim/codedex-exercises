9de357f2887530a960b73f8223093eed3492f938
ifxaamorim <reservaamorim47@gmail.com>
2025-01-06 20:43:41 -0300

    Exercicío sobre uso de loops em lista v2

--- exercises list/mixtape.py
+++ exercises list/mixtape.py
@@ -1,5 +1,5 @@
 ## Crie um programa mixtape.py e pense em um tema para as músicas. Faça uma lista playliste coloque algumas músicas.
-
+## Agora faça um loop na lista e imprima tudo!
 # Músicas pop
 
 playlist = [
