74a940f43a165700cc000f3096da881aecfbad8e
ifxaamorim <reservaamorim47@gmail.com>
2025-01-06 20:11:49 -0300

    Exercicíos com métodos de listas

---
+++ exercises list/leituras.py
@@ -0,0 +1,12 @@
+# Vamos começar um clube do livro fazendo uma lista de livros populares! 
+# O intuito deste código é usar alguns métodos de lista para adicionar e remover livros da minha lista de leituras.
+
+livros = ["Harry porter", "1984", "Life in Code", "The Mom Test", "The Fault in Our Stars"]
+
+livros.append("Pachinko")
+
+livros.remove("The Fault in Our Stars")
+
+livros.pop(1)
+
+print(livros)
