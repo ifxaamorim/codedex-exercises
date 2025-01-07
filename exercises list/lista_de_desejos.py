c83d6663b5899aa6afd251a689a3efde1cf1b635
ifxaamorim <reservaamorim47@gmail.com>
2025-01-06 21:01:42 -0300

    Exercicío: lista de desejo usando lista

---
+++ exercises list/lista_de_desejos.py
@@ -0,0 +1,19 @@
+# Crie um programa bucket_list.py com sua própria lista de buckets exclusiva.
+# Depois, crie sua lista e adicione coisas que você quer fazer na sua vida.
+## itere sobre a lista e imprima tudo
+
+minha_listaDJ = [
+
+    " 🎉 - ir em 3 festas",
+    " ✈️ - sair com familia",
+    " 🎬 - ir ao cinema",
+    " 📳 - comprar um celular novo",
+    " 💻 - trocar de computador",
+    " 💻 - ser um bom programador"
+]
+
+for lista in minha_listaDJ:
+    print(lista)
+
+## for desejo in range(len(minha_listaDJ)):
+    ## print(minha_listaDJ[desejo])
