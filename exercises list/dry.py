f0c065c64345cc4218f96109ef90adcb99c829ea
ifxaamorim <reservaamorim47@gmail.com>
2025-01-10 18:38:08 -0300

    Teste de duas funções

---
+++ exercises list/dry.py
@@ -0,0 +1,8 @@
+## Estudo de funções
+
+## Aqui estão a primira função
+
+print(chr(455)) ## Retorna um caractere apontado por um inteiro
+
+print(bin(58555)) ## Converte o int em uma string binário com prefixo OB
+
