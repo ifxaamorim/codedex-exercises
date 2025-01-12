c08848d04e4463b6df57090699e347ef9091fc58
ifxaamorim <reservaamorim47@gmail.com>
2025-01-12 20:26:28 -0300

    Noções de argumento e parâmetro

---
+++ exercises list/feliz_aniver.py
@@ -0,0 +1,8 @@
+
+def feliz_aniver(nome= "Pedro" ): ## nome = parâmentro
+    print("Feliz aniversário")
+    print("Feliz aniversário")
+    print("Feliz aniversário meu caro amigo " + nome ) ## nome = argumento
+    print("Feliz aniversário para você")
+
+feliz_aniver()
