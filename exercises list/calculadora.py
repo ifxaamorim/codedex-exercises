4c952cf8c75c013657f96eebeac27af8f44afedb
ifxaamorim <reservaamorim47@gmail.com>
2025-01-16 20:59:48 -0300

    Exercicio de função usando return

---
+++ exercises list/calculadora.py
@@ -0,0 +1,32 @@
+## Crie um programa que defina estas cinco funções: adicão, subtração, multiplicação, divisão e potência de a,b e imprima os resultados
+
+
+def soma(a,b):
+    return a + b
+
+resultado = soma(5,5)
+print("Sua soma é:",resultado)
+
+def subitracao(a,b):
+    return a - b
+
+resultado = subitracao(8,3)
+print("Sua subtração é:", resultado)
+
+def mutiplicacao(a,b):
+    return a * b
+
+resultado = mutiplicacao(15,2)
+print("Sua multiplicação é:",resultado)
+
+def divisao(a,b):
+    return a / b
+
+resultado = divisao(90,5)
+print("Sua divisão é:", resultado)
+
+def potencia(a,b):
+    return a ** b
+
+resultado = potencia(4,2)
+print("Sua potência é:", resultado)
