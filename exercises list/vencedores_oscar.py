1e26ea1c55599ec87ced5ff1847b578736bba4b3
ifxaamorim <reservaamorim47@gmail.com>
2025-01-21 20:17:34 -0300

    Desafio 2 - Listas

---
+++ exercises list/vencedores_oscar.py
@@ -0,0 +1,20 @@
+best_pictures = [
+  '2019 - Parasite',
+  '2018 - Green Book',
+  '2017 - The Shape of Water',
+  '2016 - Moonlight',
+  '2015 - Spotlight',
+  '2014 - Birdman',
+  '2013 - 12 Years a Slave',
+  '2012 - Argo',
+  '2011 - The Artist',
+  '2010 - The King\'s Speech'
+]
+
+best_pictures.insert(0, "2022 - Everything Everywhere All at Once")
+best_pictures.insert(1, "2021 - CODA")
+best_pictures.insert(2, "2020 - Nomadland")
+
+print(best_pictures)
+print(best_pictures[0:3])
+
